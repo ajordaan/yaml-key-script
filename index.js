98368846429c9657c86406047290fecdfe1299c1
Andrew Jordaan <andrew.jordaan@platform45.com>
2021-11-08 21:40:07 +0200

    Use newly added module in index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const yaml = require('js-yaml');
 const fs = require('fs');
-
+const yamlKeyAdder = require('./YamlKeyAdder');
 var argv = require('minimist')(process.argv.slice(2));
 
 try {
@@ -22,45 +21,11 @@ try {
     console.log("Cannot find source file: " + sourceFile);
     return;
   }
+  const newYaml = yamlKeyAdder.addOrReplaceKeys(fs.readFileSync(sourceFile, 'utf8'), yamlKeyAdder.readMissingKeys(missingKeysFile));
 
-  AddOrReplaceKeys(sourceFile, destFile, missingKeysFile);
+  fs.writeFile(destFile, newYaml, () => { console.log(`Output written to ${destFile}`) });
 
 } catch (e) {
   console.log(e);
 }
 
-function AddOrReplaceKeys(sourceFile, destFile, missingKeysFile = "missing_keys.txt") {
-  const doc = yaml.load(fs.readFileSync(sourceFile, 'utf8'));
-  const missingKeys = readMissingKeys(missingKeysFile);
-
-  for (const key of missingKeys) {
-    addMissingKey(doc, key)
-  }
-
-  fs.writeFile(destFile, yaml.dump(doc, { forceQuotes: true, quotingType: '"' }), () => { console.log("done") });
-}
-
-function addMissingKey(doc, pathStr) {
-  let obj = doc;
-  let childKey = null;
-  let parentObj = null;
-  let keyPath = pathStr.split('.');
-  let parentKeys = keyPath.length;
-  for (let i = 0; i < parentKeys; i++) {
-
-    parentObj = obj
-    if (!obj[keyPath[i]]) {
-      obj[keyPath[i]] = {};
-    }
-    obj = obj[keyPath[i]];
-
-    childKey = keyPath[i];
-  }
-  parentObj[childKey] = null;
-
-  console.log("Leaf Key: " + childKey);
-}
-
-function readMissingKeys(filename = "missing_keys.txt") {
-  return fs.readFileSync(filename).toString().split("\n");
-}
