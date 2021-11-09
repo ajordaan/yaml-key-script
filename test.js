34bcdbb9910cce16a0a83fe9e03057155e30a11f
Andrew Jordaan <andrew.jordaan@platform45.com>
2021-11-09 08:50:00 +0200

    Added more tests, fixed formatting

--- test.js
+++ test.js
@@ -1,29 +1,25 @@
 const yamlKeyAdder = require("./YamlKeyAdder");
 
 test('Add a new key to root', () => {
-    const yaml = `root:
+  const yaml = `root:
     key1: null`;
 
-    const missingKeys = ['root.key2']
-    const output = `root:
+  const missingKeys = ['root.key2']
+  const output = `root:
   key1: null
   key2: null
 `;
 
-    const res = yamlKeyAdder.addOrReplaceKeys(yaml, missingKeys);
-
-    console.log(res);
-    expect(res).toBe(output);
-
-
+  const res = yamlKeyAdder.addOrReplaceKeys(yaml, missingKeys);
+  expect(res).toBe(output);
 });
 
 test('Add a new nested key to root', () => {
-    const yaml = `root:
+  const yaml = `root:
         key1: null`;
 
-    const missingKeys = ['root.grandparent.parent.child1', 'root.grandparent.parent.child2']
-    const output = `root:
+  const missingKeys = ['root.grandparent.parent.child1', 'root.grandparent.parent.child2']
+  const output = `root:
   key1: null
   grandparent:
     parent:
@@ -31,22 +27,18 @@ test('Add a new nested key to root', () => {
       child2: null
 `;
 
-    const res = yamlKeyAdder.addOrReplaceKeys(yaml, missingKeys);
-
-    console.log(res);
-    expect(res).toBe(output);
-
-
+  const res = yamlKeyAdder.addOrReplaceKeys(yaml, missingKeys);
+  expect(res).toBe(output);
 });
 
-test('Add a new nested key to existing key', () => {
-    const yaml = `root:
+test('Add a new nested key to existing parent', () => {
+  const yaml = `root:
         key1:
           key2:
             existing_child: null`;
 
-    const missingKeys = ['root.key1.key2.parent.child1', 'root.key1.key2.parent.child2']
-    const output = `root:
+  const missingKeys = ['root.key1.key2.parent.child1', 'root.key1.key2.parent.child2']
+  const output = `root:
   key1:
     key2:
       existing_child: null
@@ -55,10 +47,40 @@ test('Add a new nested key to existing key', () => {
         child2: null
 `;
 
-    const res = yamlKeyAdder.addOrReplaceKeys(yaml, missingKeys);
+  const res = yamlKeyAdder.addOrReplaceKeys(yaml, missingKeys);
+  expect(res).toBe(output);
+});
 
-    console.log(res);
-    expect(res).toBe(output);
+test('Null out existing key', () => {
+  const yaml = `root:
+      key1:
+        key2:
+          existing_child: "I have a value"
+`;
 
+  const missingKeys = ['root.key1.key2.existing_child']
+  const output = `root:
+  key1:
+    key2:
+      existing_child: null
+`;
 
+  const res = yamlKeyAdder.addOrReplaceKeys(yaml, missingKeys);
+  expect(res).toBe(output);
+});
+
+test('Null out existing parent', () => {
+  const yaml = `root:
+      key1:
+        key2:
+          existing_child: "I have a value"
+`;
+
+  const missingKeys = ['root.key1']
+  const output = `root:
+  key1: null
+`;
+
+  const res = yamlKeyAdder.addOrReplaceKeys(yaml, missingKeys);
+  expect(res).toBe(output);
 });
